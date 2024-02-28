e8d15412c27f9424dd38c0b4bd70703152c79a29
Marcelo Xavier de Almeida <40306608+Marceloxav@users.noreply.github.com>
2024-02-27 21:41:27 -0300

    Add files via upload

---
+++ popup.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0, max-scale=1.0"><meta http-equiv="refresh" content="100"/>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="marcelo" >
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=default"></script>
+
+</head>
+<title>GASOLINA RESPONDE CERTO 3</title>
+<script type="text/javascript">
+var userAgent = navigator.userAgent.toLowerCase();
+var devices = new Array('nokia','iphone','blackberry','sony','lg',
+'htc_tattoo','samsung','symbian','SymbianOS','elaine','palm',
+'series60','windows ce','android','obigo','netfront',
+'openwave','mobilexplorer','operamini');
+var url_redirect = 'm.seusite.com';
+function mobiDetect(userAgent, devices) {
+for(var i = 0; i < devices.length; i++) {
+if (userAgent.search(devices[i]) > 0) {
+return true;
+}
+}
+return false;
+}
+if (mobiDetect(userAgent, devices)) {
+window.location.href = url_redirec</script></p>
+
+<p></p>
+<style type="text/css">body {
+  font-family: Arial;
+  font-size: 17px;
+  padding: 8px;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.row {
+  display: -ms-flexbox; /* IE10 */
+  display: flex;
+  -ms-flex-wrap: wrap; /* IE10 */
+  flex-wrap: wrap;
+  margin: 0 -16px;
+}
+
+.col-25 {
+  -ms-flex: 25%; /* IE10 */
+  flex: 25%;
+}
+
+.col-50 {
+  -ms-flex: 50%; /* IE10 */
+  flex: 50%;
+}
+
+.col-75 {
+  -ms-flex: 75%; /* IE10 */
+  flex: 75%;
+}
+
+.col-25,
+.col-50,
+.col-75 {
+  padding: 0 16px;
+}
+
+.container {
+  background-color: #f2f2f2;
+  padding: 5px 20px 15px 20px;
+  border: 1px solid lightgrey;
+  border-radius: 3px;
+}
+
+input[type=text] {
+  width: 100%;
+  margin-bottom: 20px;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+}
+
+label {
+  margin-bottom: 10px;
+  display: block;
+}
+
+.icon-container {
+  margin-bottom: 20px;
+  padding: 7px 0;
+  font-size: 24px;
+}
+
+.btn {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px;
+  margin: 10px 0;
+  border: none;
+  width: 100%;
+  border-radius: 3px;
+  cursor: pointer;
+  font-size: 17px;
+}
+
+.btn:hover {
+  background-color: #45a049;
+}
+
+a {
+  color: #2196F3;
+}
+
+hr {
+  border: 1px solid lightgrey;
+}
+
+span.price {
+  float: right;
+  color: grey;
+}
+
+/* Responsive layout - when the screen is less than 800px wide, make the two columns stack on top of each other instead of next to each other (also change the direction - make the "cart" column go on top) */
+media (max-width: 800px) {
+  .row {
+    flex-direction: column-reverse;
+  }
+  .col-25 {
+    margin-bottom: 20px;
+  }
+}
+</style>
+<style type="text/css">.profile{
+    width: 400px;
+    height: 400px;
+   background-size: 150px 150px;
+background-size: cover
+border-radius: 50%;
+margin:auto;
+}
+
+.profile img{
+    width: auto;
+align: justify;
+}
+</style>
+
+<center><H5>xxxxx-5</H5></center>
+<!-- Conteúdo do pop-up de assinatura -->
+<div id="popup" class="popup">
+    <div class="popup-content">
+        <span class="close" onclick="fecharPopup()">&times;</span>
+        <h2>Assine nossa newsletter</h2>
+        <form action="processar_assinatura.php" method="post">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+            <button type="submit">Assinar</button>
+        </form>
+    </div>
+</div>
+</style>
+	<script>
+	// Função para abrir o pop-up
+function abrirPopup() {
+    document.getElementById("popup").style.display = "block";
+}
+
+// Função para fechar o pop-up
+function fecharPopup() {
+    document.getElementById("popup").style.display = "none";
+}
+</script>
+</body>
+</html>
+	
